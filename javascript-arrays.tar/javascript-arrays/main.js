3229e80ae41749193cc5611878009ea2c612d2a6
Lambert Nguyen <lambertdknguyen@yahoo.com>
2023-10-27 04:00:16 +0000

    fixed last part of javascript-arrays

--- javascript-arrays.tar/javascript-arrays/main.js
+++ javascript-arrays.tar/javascript-arrays/main.js
@@ -10,3 +10,10 @@ console.log(
   'Mexico is ' + colors[0] + ', ' + colors[1] + ', ' + 'and ' + colors[2]
 );
 console.log(colors);
+
+const students = ['John', 'Paul', 'George', 'Ringo'];
+const numberOfStudents = students.length;
+console.log('There are ' + numberOfStudents + ' students in the class.');
+const lastIndex = students.length - 1;
+const lastStudent = lastIndex[students.length - 1];
+console.log('The last student in the class is ' + lastStudent);
