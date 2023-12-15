26088fa84bf2980dac64afe858cc01daa9338211
Lambert Nguyen <lambertdknguyen@yahoo.com>
2023-12-15 00:27:36 +0000

    edited the code

--- dom-events.tar/dom-events/buttons.js
+++ dom-events.tar/dom-events/buttons.js
@@ -15,13 +15,13 @@ function handleMouseOver(event){
 }
  const element2 =document.querySelector('.hover-button');
   if (!$hoverButton) throw new Error('The $hoverButton query failed');
-  element2.addEventListener('mouseover',event)
+  element2.addEventListener('mouseover',handleMouseOver)
 
 function handleDoubleClick(event){
   console.log('button doubled-clicked');
   console.log(event);
   console.log(event.target);
 }
- const element3=document.querySelector(.'double-click-button');
+ const element3=document.querySelector('double-click-button');
   if (!$dblClickButton) throw new Error('The $dblClickButton query failed');
   element3.addEventListener('dblclick', handleDoubleClick);
