dd75acb3b858784296a2e767ec74f37a07c222e6
Lambert Nguyen <lambertdknguyen@yahoo.com>
2023-12-17 23:43:49 +0000

    js-lightbulb

--- javascript-light-bulb.tar/javascript-light-bulb/main.js
+++ javascript-light-bulb.tar/javascript-light-bulb/main.js
@@ -1,15 +1,6 @@
-const container=document.getElementById('container');
-const circle=document.getElementById('circle');
-
-container.addEventListener('mouseover', function handleMouseOver(){
-  container.style.backgroundColor="beige";
-});
-circle.addEventListener('mouseover', function handleMouseOver(){
-  circle.style.backgroundColor="yellow";
-});
-container.addEventListener('mouseout', function handleMouseOut(){
-  container.style.backgroundColor="black";
-});
-container.addEvenetListner('mouseout', function handleMouseOut(){
-  circle.style.backgroundColor="gray";
-});
+function changeStyle(){
+  const element=document.getElementById("container");
+  const element2=document.getElementById("button");
+  element.style.backgroundColor="beige";
+  element2.style.backgroundColor="yellow";
+}
