3efa2c9f6056b6d35f8458a5fb303c60c017f104
Lambert Nguyen <lambertdknguyen@yahoo.com>
2023-12-28 01:46:37 +0000

    string-manipulation-3

--- string-manipulation-3.tar/string-manipulation-3/src/title-case.js
+++ string-manipulation-3.tar/string-manipulation-3/src/title-case.js
@@ -1,1 +1,7 @@
-/* exported titleCase */
+function titleCase(string) {
+  return string
+    .toLowerCase()
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
