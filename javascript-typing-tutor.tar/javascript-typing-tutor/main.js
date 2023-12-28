b1b7a36068662d9054f004290e8c2f352d686f56
Lambert Nguyen <lambertdknguyen@yahoo.com>
2023-12-28 05:15:45 +0000

    IP

---
+++ javascript-typing-tutor.tar/javascript-typing-tutor/main.js
@@ -0,0 +1,11 @@
+let cursor = true;
+const speed = 250;
+setInterval(() => {
+  if (cursor) {
+    document.getElementById('cursor').style.opacity = 0;
+    cursor = false;
+  } else {
+    document.getElementById('cursor').style.opacity = 1;
+    cursor = true;
+  }
+}, speed);
