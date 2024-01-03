0c3622df96e04f18d2101cc0e90e81a066805473
Lambert Nguyen <lambertdknguyen@yahoo.com>
2024-01-03 03:27:14 +0000

    fixed dom-event-delegation

--- dom-event-delegation.tar/dom-event-delegation/main.js
+++ dom-event-delegation.tar/dom-event-delegation/main.js
@@ -1,8 +1,13 @@
 const taskList = document.querySelector('.task-list'); // select the button element
 
-taskList.addEventListener('click', function(event){
-  console.log('event.target:',event.target);
-  console.log('event.target.tagName:',event.target.tagName);
-  console.log(event.target.closest('.task-list-item'));
-  console.log(event.target.remove(event.target.closest('.task-list-item')));
+taskList.addEventListener('click', function (event) {
+  console.log('event.target:', event.target);
+  console.log('event.target.tagName:', event.target.tagName);
+  if (event.target.tagName !== 'BUTTON') {
+    return;
+  }
+
+  const taskListItem = event.target.closest('.task-list-item');
+  console.log('closest .task-list-item', taskListItem);
+  taskListItem.remove();
 });
