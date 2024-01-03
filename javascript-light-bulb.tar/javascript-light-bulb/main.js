ad7cf93fd2847a65cddecb91a7a7f2b26e6b5683
Lambert Nguyen <lambertdknguyen@yahoo.com>
2024-01-03 22:54:37 +0000

    fixed the code

--- javascript-light-bulb.tar/javascript-light-bulb/main.js
+++ javascript-light-bulb.tar/javascript-light-bulb/main.js
@@ -1,10 +1,13 @@
-
-   function mouseOver() {
-     document.getElementById('container').style.backgroundColor = 'black';
-     document.getElementById('circle').style.backgroundColor = 'gray';
-   }
-
-   function mouseOut() {
-     document.getElementById('container').style.backgroundColor = 'beige';
-     document.getElementById('circle').style.backgroundColor = 'yellow';
-   }
+let isOn=false;
+const ceiling=document.querySelector('.ceiling');
+const lightbulb=document.querySelector('.lightbulb')
+lightbulb.addEventListener('click',function (event){
+  isOn=!isOn;
+  if(isOn){
+    ceiling.className='ceiling is-on';
+    lightbulb.className='lightbulb is-on'
+  }else{
+    ceiling.className='ceiling';
+    lightbulb.className='lightbulb'
+  }
+})
