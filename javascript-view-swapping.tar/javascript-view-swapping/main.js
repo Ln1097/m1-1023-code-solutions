fdbc9fd356586fed6e360ee26cd27348a306642c
Lambert Nguyen <lambertdknguyen@yahoo.com>
2024-01-05 15:15:46 +0000

    IP

--- javascript-view-swapping.tar/javascript-view-swapping/main.js
+++ javascript-view-swapping.tar/javascript-view-swapping/main.js
@@ -1,3 +1,26 @@
 const tab=document.querySelector(".tab-container")
-const tabList=document.querySelectorAll("tab")
-cont viewList=document.querySelectorAll("view")
+const tabList=document.querySelectorAll(".tab")
+const viewList=document.querySelectorAll(".view")
+tab.addEventListener("click",handleViewSwap);
+function handleViewSwap(event){
+  if (!event.target.matches('.tab')){
+    return;
+  }
+  for (let tabIndex=0; tabIndex<tabList.length; tabIndex++){
+    if(tabList[tabIndex]===event.target){
+      tabList[tabIndex].className='tab active';
+    }else{
+      tabList[tabIndex].className='tab';
+    }
+  }
+}
+
+const viewName=event.target.getAttribute('data-view');
+
+for(let viewIndex=0; viewIndex<viewList.length; viewIndex++){
+  if(viewList[viewIndex].getAttribute('data view')!==viewName){
+    viewList[viewIndex].className='view hidden';
+  }else{
+    viewList[viewIndex].className='view';
+  }
+}
