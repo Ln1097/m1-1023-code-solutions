597b4d17a03a6fa7cccd1f71ccd6b9765f409242
Lambert Nguyen <lambertdknguyen@yahoo.com>
2024-01-08 23:31:37 +0000

    javascript-typing-tutor

--- javascript-typing-tutor.tar/javascript-typing-tutor/main.js
+++ javascript-typing-tutor.tar/javascript-typing-tutor/main.js
@@ -1,11 +1,33 @@
-let cursor = true;
-const speed = 250;
-setInterval(() => {
-  if (cursor) {
-    document.getElementById('cursor').style.opacity = 0;
-    cursor = false;
-  } else {
-    document.getElementById('cursor').style.opacity = 1;
-    cursor = true;
+const typeSentenceDisplay=document.getElementbyID('typeSentence')
+const sentenceFieldDisplay=document.getElementById('sentenceField')
+
+sentenceFieldDisplay.addEventListener('input',()=>{
+  const arraySentence=typeSentenceDisplay.querySelectorAll('span')
+  const arrayType = sentenceFieldDisplay.value.split('')
+  arraySentence.forEach((characterSpan, index)=>{
+    const character=arrayType[index]
+    if (character==null){
+      characterSpan.classList.remove('correct');
+      characterSpan.classList.remove('incorrect');
+    }else if (character===characterSpan.innerText){
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect');
+    }else{
+      characterSpan.classList.remove('correct');
+      characterSpan.classList.add('incorrect');
+    }
+  })
+})
+
+function sentenceStyle() {
+  const sentence=function sentenceColor() {
+    typeSentenceDisplay.innerHTML=''
   }
-}, speed);
+  sentence.split('').forEach((character)=>{
+    const characterSpan = document.createElement('span');
+    characterSpan.innerText = character;
+    typeSentenceDisplay.appendChild(characterSpan);
+  })
+  sentenceFieldDisplay.value = null;
+}
+sentenceStyle()
