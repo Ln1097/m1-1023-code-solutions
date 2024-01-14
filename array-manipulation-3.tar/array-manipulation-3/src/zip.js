ef248b8c576ff9a9586e66e7ead8fdd071c79414
Lambert Nguyen <lambertdknguyen@yahoo.com>
2024-01-14 00:26:18 +0000

    finished array-manipulation-3

--- array-manipulation-3.tar/array-manipulation-3/src/zip.js
+++ array-manipulation-3.tar/array-manipulation-3/src/zip.js
@@ -1,10 +1,10 @@
-/* exported zip */
-const zip = (first, second) => {
-  const longerLength = Math.max(first.length, second.length);
-  const indices = [...new Array(longerLength).keys()];
+function zip(first, second) {
+  const zipped = [];
 
-  return indices.reduce(
-    (list, index) => [...list, [first[index], second[index]]],
-    []
-  );
-};
+  for (let index = 0; index < first.length && index<second.length; index++) {
+      zipped.push([first[index], second[index]]);
+  }
+
+  return zipped;
+}
+console.log(zip)
